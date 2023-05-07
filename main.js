67ce415578ce8a14c3e0aec7121347e0cb852a60
Luana Santini <luanasantini3@gmail.com>
2023-05-07 18:21:23 -0300

    Atualizações estéticas e com listas de robotrons para escolher a cor.

--- main.js
+++ main.js
@@ -1,25 +1,78 @@
-const somar = document.querySelector('#somar');
-const subtrair = document.querySelector('#subtrair');
-const braco = document.querySelector('#braco');
+const estatisticas = document.querySelectorAll('[data-estatistica')
+const peca = document.querySelector('[data-peca]');
+const botaoDeControle = document.querySelectorAll('[data-controle]');
+const coresRobotron = document.querySelectorAll('[data-cor]')
+const robotronExibido = document.querySelector('.robotron-exibido');
 
-const botaoDeControle = document.querySelectorAll('.controle-ajuste');
-
-console.log(botaoDeControle)
-console.log(somar)
-console.log(subtrair)
-console.log(braco)
-
-botaoDeControle.forEach( (elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+coresRobotron.forEach((imagem, cor) => {
+    imagem.addEventListener("click", (evento) => {
+        atualizaRobotron(cor);
     })
 })
 
+const atualizaRobotron = function (cor) {
+    imagemAtual = cor;
+    const imagem = coresRobotron[imagemAtual].getAttribute('src');
+    robotronExibido.setAttribute('src', imagem);
+}
+botaoDeControle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca, evento.target.dataset.controle)
+    })
+})
 
-const manipulaDados = function(operacao) {
-    if(operacao === "-") {
-        braco.value = parseInt(braco.value) - 1
+const manipulaDados = function (operacao, controle) {
+
+    const peca = controle.querySelector('[data-contador]');
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+const atualizaEstatisticas = function (peca, operacao) {
+    estatisticas.forEach((elemento) => {
+        if (operacao === "-") {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        } else {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        }
+    })
+}
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
